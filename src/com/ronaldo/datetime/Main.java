bd70dc9207c864994259464332d74c717a8e9331
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2022-11-14 19:05:45 -0300

    created LocalDate, LocalDateTime, Instant

--- src/com/ronaldo/datetime/Main.java
+++ src/com/ronaldo/datetime/Main.java
@@ -3,24 +3,19 @@
  */
 package com.ronaldo.datetime;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
- * @author ronal
- *
+ * @author Ronaldo Gomes do Nascimento
+ * Esse é um treinamento do professor Nélio Alves sobre data e hora no Java
  */
 public class Main {
-
-	/**
-	 * 
-	 */
-	public Main() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LocalDate date01 = LocalDate.now(); System.out.println("local Date: "+date01 );
+		LocalDateTime date02 = LocalDateTime.now(); System.out.println("local Date Time: "+date02 );
+		Instant date03 = Instant.now(); System.out.println("Instant: "+date03 );
 
 	}
 
