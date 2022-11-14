365a0c1a5d7039d40c7177c76e72e5078fd4ca2c
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2022-11-14 20:16:04 -0300

    Local Date,LocalDateTime, Instant a partir de texto ISO8601, Instant local para GMT

--- src/com/ronaldo/datetime/Main.java
+++ src/com/ronaldo/datetime/Main.java
@@ -15,7 +15,11 @@ public class Main {
 	public static void main(String[] args) {
 		LocalDate date01 = LocalDate.now(); System.out.println("local Date: "+date01 );
 		LocalDateTime date02 = LocalDateTime.now(); System.out.println("local Date Time: "+date02 );
-		Instant date03 = Instant.now(); System.out.println("Instant: "+date03 );
+		Instant date03 = Instant.now(); System.out.println("Gera em GMT - Instant: "+date03 );
+		LocalDate date04 = LocalDate.parse("2022-11-14");System.out.println("Data a partir de texto - ISO8601 "+date04);
+		LocalDateTime date05 = LocalDateTime.parse("2022-11-14T01:30:26");System.out.println("Local Date Time a partir de texto "+date05);
+		Instant date06 = Instant.parse("2022-11-14T01:30:26Z");System.out.println("Instant a partir de texto "+date06);
+		Instant date07 = Instant.parse("2022-11-14T01:30:26-03:00");System.out.println("Instant a partir de texto GMT "+date07);
 
 	}
 
