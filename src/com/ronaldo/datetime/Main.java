837cd10e80667910ea80522e7ed7c6eb4729de22
Ronaldo Gomes do Nascimento <ronaldogdn@gmail.com>
2022-11-14 18:41:38 -0300

    created project

---
+++ src/com/ronaldo/datetime/Main.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.ronaldo.datetime;
+
+/**
+ * @author ronal
+ *
+ */
+public class Main {
+
+	/**
+	 * 
+	 */
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
